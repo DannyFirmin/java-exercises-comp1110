50fe7f754451f967cc055a6c6afad334557beb9b
dannyfeng (Yanming) <u6611178@anu.edu.au>
2018-02-20 21:17:04 +1100

    broke program

--- src/comp1110/lab1/HelloWorld.java
+++ src/comp1110/lab1/HelloWorld.java
@@ -2,6 +2,6 @@ package comp1110.lab1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Goodbye world!");
     }
 }
