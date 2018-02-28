b306c962434448b7e33d807962211e2d69ef99cb
dannyfeng (Yanming) <u6611178@anu.edu.au>
2018-02-28 16:09:38 +1100

    Done with Countries in Lab 2

---
+++ src/comp1110/lab2/Countries.java
@@ -0,0 +1,16 @@
+package comp1110.lab2;
+
+public class Countries {
+    public static void main(String[] args) {
+        String s[];
+        s = new String[4];
+        s[0] = "Germany";
+        s[1] = "Argentina";
+        s[2] = "Netherlands";
+        s[3] = "Brazil";
+        System.out.println(s[0]);
+        System.out.println(s[1]);
+        System.out.println(s[2]);
+        System.out.println(s[3]);
+    }
+}
