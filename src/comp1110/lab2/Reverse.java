28a2a6594a9b50f7ccf4e35e8456252c1af6a447
dannyfeng (Yanming) <u6611178@anu.edu.au>
2018-02-28 16:10:08 +1100

    Done with Reverse in Lab 2

---
+++ src/comp1110/lab2/Reverse.java
@@ -0,0 +1,11 @@
+package comp1110.lab2;
+
+public class Reverse {
+    public static void main(String[] args) {
+        int i = 30;
+        while (i >= 10) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
