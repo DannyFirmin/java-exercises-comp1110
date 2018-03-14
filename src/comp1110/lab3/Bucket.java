e4012ec13fb946647b91b3a7dce6ecda5b7dddd8
Danny Feng <u6611178@anu.edu.au>
2018-03-14 15:45:25 +1100

    start lab 3

---
+++ src/comp1110/lab3/Bucket.java
@@ -0,0 +1,11 @@
+package comp1110.lab3;
+
+public class Bucket {
+    private double capacity;
+    private double contents;
+
+    public Bucket(double capacity) {
+        this.capacity = capacity;
+    }
+
+}
