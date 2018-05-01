dd9451afaf9f25d6b5b91d800de8065fb4208f3b
Danny Feng (Yanming) <u6611178@anu.edu.au>
2018-05-02 09:31:28 +1000

    start lab6

---
+++ src/comp1110/lab6/Board.java
@@ -0,0 +1,37 @@
+package comp1110.lab6;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.stage.Stage;
+
+public class Board extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Board");
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root,600,519);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        Polygon triangle = new Polygon(0,-86.6,100,86.6,-100,86.6);//where to put postion(0,0)?
+        triangle.setFill(Color.LIGHTGREY);
+        root.getChildren().add(triangle);
+
+        class Triangle extends Polygon{
+            Triangle(double x, double y, double side){
+                Polygon triangle = new Polygon(x,y,side,side);// centered at (x,y), with sides of length side.
+            }
+        }
+
+
+    }
+}
