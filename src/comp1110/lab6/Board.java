d2962acf207d9cfe43aef4f3068b8515cd4267c2
Danny Feng (Yanming) <u6611178@anu.edu.au>
2018-05-02 22:01:54 +1000

    finished lab 6

--- src/comp1110/lab6/Board.java
+++ src/comp1110/lab6/Board.java
@@ -1,6 +1,5 @@
 package comp1110.lab6;
 
-import comp1110.lab5.board2;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -55,12 +54,14 @@ public class Board extends Application {
     class Triangle extends Polygon {
         Triangle(double x, double y, double side) {
             super(side / 2, Math.sqrt(Math.pow(side, 2) - Math.pow(side / 2, 2)) / 2, -1 * side / 2, Math.sqrt(Math.pow(side, 2) - Math.pow(side / 2, 2)) / 2, 0, -1 * Math.sqrt(Math.pow(side, 2) - Math.pow(side / 2, 2)) / 2);
-            
+
             this.setLayoutX(x);
             this.setLayoutY(y);
             this.setFill(Color.LIGHTGREY);
 
             double h = side * Math.sin(Math.PI * 60 / 180);
+
+
 
             double x1 = x;
             double y1 = y + h / 2;
@@ -97,3 +98,58 @@ public class Board extends Application {
 //                        (-(side/2)), Math.sqrt((side*side)-((side/2)*(side/2)))/2);
 //            }
 //        }
+
+
+//package comp1110.lab6;
+//
+//        import javafx.application.Application;
+//        import javafx.scene.Group;
+//        import javafx.scene.Scene;
+//        import javafx.scene.paint.Color;
+//        import javafx.scene.shape.Polygon;
+//        import javafx.stage.Stage;
+//
+//        import java.util.ArrayList;
+//
+//
+//public class board2 extends Application {
+//    class Triangle extends Polygon {
+//        Triangle(double x, double y, double side) {
+//            super(side / 2, Math.sqrt(Math.pow(side, 2) - Math.pow(side / 2, 2)) / 2, -1 * side / 2, Math.sqrt(Math.pow(side, 2) - Math.pow(side / 2, 2)) / 2, 0, -1 * Math.sqrt(Math.pow(side, 2) - Math.pow(side / 2, 2)) / 2);
+//            super.setLayoutX(x);
+//            super.setLayoutY(y);
+//            super.setFill(Color.LIGHTGREY);
+//        }
+//
+//
+//    }
+//
+//    @Override
+//    public void start(Stage primaryStage) throws Exception {
+//        primaryStage.setMaxWidth(600);
+//        primaryStage.setWidth(600);
+//        primaryStage.setMaxHeight(519);
+//        primaryStage.setHeight(519);
+//        Group root = new Group();
+//
+//        ArrayList<Triangle> list = new ArrayList<>();
+//
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = -2; j < 3; j++) {
+//                Triangle t = new Triangle(j * 100 + 300, 86.6 + 173.2 * i, 196);
+//                if (Math.abs(j % 2) == 1) {
+//                    t.setRotate(180);
+//                }
+//                list.add(t);
+//            }
+//        }
+//
+//        for (Triangle t : list) {
+//            root.getChildren().addAll(t);
+//        }
+//        primaryStage.setScene(new Scene(root));
+//        primaryStage.show();
+//    }
+//
+//
+//}
